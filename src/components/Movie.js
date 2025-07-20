fe1ac8677770a4b93b83e672d24955557cbc1d20
far1h <farihmuhammad45@gmail.com>
2025-07-20 21:35:26 +0700

    Display individual movie details, including title, release date, runtime, rating, and description.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,27 @@
+import { use, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const Movie = () => {
+    const [movieId, setMovieId] = useState({});
+    let { id } = useParams();
+
+    useEffect(() => {
+        let myMovie = {
+            id: 1,
+                title: "Sample Movie",
+                release_date: "2024-01-01",
+                runtime: 120,
+                mpaa_rating: "PG-13",
+                description: "This is a sample movie description."
+        }
+        setMovieId(myMovie);
+    }, [id]);
     return (
         <>
-            <h2>Movie</h2>
+            <h2>Movie: {movieId.title}</h2>
+            <small><em>{movieId.release_date}, {movieId.runtime} min, Rated {movieId.mpaa_rating}</em></small>
             <hr />
+            <p>{movieId.description}</p>
         </>
     )
 }
