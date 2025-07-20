d5a24bdbb3c9f8bdce9f0ee8d660e45931178cac
far1h <farihmuhammad45@gmail.com>
2025-07-20 16:34:23 +0700

    Add application layout and navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 function App() {
   return (
-    <div>
-      Go Watch a Movie!
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Go Watch a Movie!</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!"><span className="badge bg-success">Login</span></a>
+        </div>
+        <hr className="mb-3" />
+      </div>
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">Home</a>
+              <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+        
+        </div>
+      </div>
     </div>
   );
 }
